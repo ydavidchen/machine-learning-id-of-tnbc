15995fb4400247b7ee7dcb6946a03621c108a118
David Chen <David.Y.Chen@ibm.com>
2021-05-27 20:39:40 -0700

    L1 feature selection & Sagemaker XGBoost complete

--- src/helpers.py
+++ src/helpers.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 
 ## Reusable helpers
 def reveal_bucket(bucket):
@@ -11,12 +12,26 @@ def get_s3_uri(prefix, bucket):
     """ Generates string of S3 URI """
     return "s3://{}/{}".format(bucket, prefix)
 
-def directS3Save(df, bucket, path_s3='df.csv'):
+def directS3Save(df, bucket, path_s3='df.csv', header=True, index=True):
     """ Directly exports a dataframe to CSV in a bucket """
     from io import StringIO
     import boto3
 
     csv_buffer = StringIO()
-    df.to_csv(csv_buffer)
+    df.to_csv(csv_buffer, header=header, index=index)
     s3_resource = boto3.resource('s3')
     s3_resource.Object(bucket, path_s3).put(Body=csv_buffer.getvalue())
+    
+    
+def plot_roc(ytrue, yscore):
+    """ Wrapper to plot ROC curve for binary classification """
+    from sklearn import metrics
+
+    plt.figure(dpi=100)
+
+    fpr, tpr, _ = metrics.roc_curve(ytrue, yscore)
+    auc = metrics.roc_auc_score(ytrue, yscore)
+    plt.plot(fpr,tpr,label="AUC=%.3f" % auc)
+    plt.legend(loc=4)
+
+    plt.show()
